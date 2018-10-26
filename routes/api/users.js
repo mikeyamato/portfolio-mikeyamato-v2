7524e0de73ab87a3162d5b0431c777ed4438aafa
Mix Master Mike <mikeyamato@gmail.com>
2018-10-26 09:33:11 -0700

    updated server

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 // @route GET api/users/test
 // @desc tests users route
 // @access public
 router.get('/test', (req ,res) => res.json({msg: "users works"}));
+
+// @route GET api/users/signup
+// @desc signup users
+// @access public
+router.post('/signup', (req ,res) => {
+	User.findOne({ email: req.body.email })
+	.then(user => {
+		if(user){
+			return res.status(400).json({ email: 'Email already exists' });
+		} else {
+			const newUser = new User({
+				nickname: req.body.nickname,
+				email: req.body.email,
+				password: req.body.password
+			});
+			// to hash password with 10 random characters
+			bcrypt.getSalt(10, (err,salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if(err) throw err;
+					newUser.password = hash;
+					newUser
+						.save()
+						.then(user => res.json(user))
+						.catch(err => console.log(err))
+				})
+			})
+		}
+	})
+});
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const bodyparser = require('body-parser');
 const port = process.env.PORT || 5000; 
 const db = require('./config/keys').mongoURI;
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+
+app.use(bodyparser.urlencoded({ extended:false }));
+app.use(bodyparser.json());
 
 mongoose
 	.connect(db)
